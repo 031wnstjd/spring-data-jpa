67da1a1b584d2873d6734cf4b77b9c2c612bf0d6
Junsung <031wnstjd@naver.com>
2023-09-10 01:54:11 +0900

    - modify member and add team entity - add MemberTest

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,22 +1,41 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {}
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        changeTeam(team);
+    }
+
+    public void changeTeam(Team team) {
+        if (this.team != null) {
+            team.getMembers().remove(this); // 이미 team에 소속 되어 있었다면 해당 정보 제거
+        }
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
